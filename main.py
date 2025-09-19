a202f37733600d2ccea2fa53e7b8df0889a35b57
Andrew <aalesh3@wgu.edu>
2025-09-19 00:50:06 -0700

    Working on data splitting test and training sets

--- main.py
+++ main.py
@@ -70,8 +70,8 @@ def go(config: DictConfig):
         if "data_check" in active_steps:
             # Run the data_check step
             _ = mlflow.run(
-                os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
-                "main",
+                os.path.join(hydra.utils.get_original_cwd(), "src", "train_val_test_split"),
+                'main',
                 parameters={
                     "csv": "clean_sample.csv:latest",
                     "ref": "clean_sample.csv:reference",
@@ -83,9 +83,20 @@ def go(config: DictConfig):
             pass
 
         if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
+            #Split the data into train and test sets
+            _ = mlflow.run(
+                
+                f"{config['main']['components_repository']}/train_val_test_split",
+                "main",
+                version='main',
+                env_manager="conda",
+                parameters = {
+                    "input": "clean_sample.csv:latest",
+                    "test_size": config["modeling"]["test_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"]
+                }
+             )
             pass
 
         if "train_random_forest" in active_steps:
